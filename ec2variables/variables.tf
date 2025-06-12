689cf63abf767349dd4283e478acd1eb87d054c4
Ravikanti Sai <RavikantiS@emids.com>
2025-06-12 19:55:04 +0530

    Terraform

---
+++ ec2variables/variables.tf
@@ -0,0 +1,18 @@
+variable "image_id" {
+    type = string
+    default = "ami-09c813fb71547fc4f"
+  
+}
+
+variable "instance_type" {
+    type = string
+    default = "t3.micro"
+  
+}
+
+variable "sg_id" {
+
+    default = "[ aws_security_group.allow.id ]"
+
+  
+}
