c3112ab99641283ac2a1cca7209d3641210b3a3f
Ravikanti Sai <RavikantiS@emids.com>
2025-06-12 13:28:23 +0530

    Terraform

--- ec2instance/instance.tf
+++ ec2instance/instance.tf
@@ -1,18 +1,11 @@
-resource "aws_instance" "m" {
-  
-}
-
-
-
-
-
-
 resource "aws_instance" "roboshop" {
   ami           = "ami-09c813fb71547fc4f"
   instance_type = "t3.micro"
   vpc_security_group_ids = [ aws_security_group.allow_all.id ]
   
-  
+  tags = {
+    Name = "HelloWorld"
+  }
 }
 
 resource "aws_security_group" "allow_all" {
