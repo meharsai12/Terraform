240e98894ab7e37159a73ff10e83641c08f2e964
Ravikanti Sai <RavikantiS@emids.com>
2025-06-16 16:23:06 +0530

    Terraform

---
+++ dynamic_block/instance.tf
@@ -0,0 +1,36 @@
+resource "aws_instance" "roboshop" {
+  count = 4
+  ami           = var.ami_id
+  instance_type = var.environment == "dev" ? "t3.micro" : "t3.small"
+  vpc_security_group_ids = [ aws_security_group.allow_all.id ]
+  
+  tags ={
+    Name = var.instances[count.index]
+  }
+}
+
+resource "aws_security_group" "allow_all" {
+    name        = var.sg_name
+    description = var.sg_desc
+    
+    dynamic "ingress" {
+      for_each = var.ingress_block
+      content {
+        from_port        = ingress.value["from_port"]
+        to_port          = ingress.value["to_port"]
+        protocol         = "-1"
+        cidr_blocks      = var.cidr_blocks
+        ipv6_cidr_blocks = ["::/0"]
+      }
+      
+    }
+    egress {
+        from_port        = var.fromport
+        to_port          = var.toport
+        protocol         = "-1"
+        cidr_blocks      = var.cidr_blocks
+        ipv6_cidr_blocks = ["::/0"]
+    }
+
+    tags = var.sg_tags
+}
--- dynamic_block/variables.tf
+++ dynamic_block/variables.tf
@@ -76,3 +76,23 @@ variable "domain_name" {
     default = "meharsai.site"
   
 }
+
+variable "ingress_block" {
+
+    default = [
+        {
+            from_port = 22
+            to_port = 22
+        },
+        {
+            from_port = 80
+            to_port = 80
+        },
+
+        {
+            from_port = 8080
+            to_port = 8080
+        }
+    ]
+  
+}
