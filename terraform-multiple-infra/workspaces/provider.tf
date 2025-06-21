e7c47d15f327b231a91c00a7509f009ecda5bdf5
Ravikanti Sai <RavikantiS@emids.com>
2025-06-21 19:51:19 +0530

    terraform

---
+++ terraform-multiple-infra/workspaces/provider.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.98.0"
+    }
+  }
+
+  backend "s3" {
+     bucket = "tf-workspaces32"
+    key    = "workspace"
+    region = "us-east-1"
+    #dynamodb_table = "tfprac-state-dynamodb"  # if you want to do it with dynamo db asdd dynamodb_table and table name
+    encrypt = true
+    use_lockfile = true # we can create locking by using s3 also if you want to do it with s3 just add use_lockfile
+  }
+}
+
+provider "aws" {
+  # Configuration options
+}
